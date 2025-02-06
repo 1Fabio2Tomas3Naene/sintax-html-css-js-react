ff9f49c41007fb0667181145c2bd163c7c7dcef0
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-06 14:04:41 +0200

    criei arquivos css e jsx

---
+++ sintax-languages/src/app/Components/Top/Top.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Top.module.css";
+import { IoMoonOutline } from "react-icons/io5";
+import { RxSun } from "react-icons/rx";
+
+export default function Top(){
+    return(
+
+    )
+};
