0ae5cc94366d6d717f47ec3bad9bfe7c840d9b2d
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-06 18:34:32 +0200

    criei uma funcao para alterar a tela a cada novo click

--- sintax-languages/src/app/page.js
+++ sintax-languages/src/app/page.js
@@ -1,10 +1,16 @@
+'use client';
 import styles from "./page.module.css";
 import Top from "./Components/Top/Top";
+import { useState } from "react";
 
 export default function Home() {
+  const [lightMode, setLightMode] = useState(false);
+  const DarkMode = () =>{
+    setLightMode(!lightMode);
+  }
   return (
     <div>
-      <Top />
+      <Top standardModeLightMode={lightMode} onClickSetLightMode={DarkMode}/>
     </div>
   );
 }
