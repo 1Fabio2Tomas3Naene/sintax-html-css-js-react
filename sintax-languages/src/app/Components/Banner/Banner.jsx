ccc9f7d10f47168d4304c9463794970bfdae0ed4
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-06 18:41:32 +0200

    criei uma pasta com arquivos la dentro e preperei os arquivos para a codificacao

---
+++ sintax-languages/src/app/Components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./Banner.module.css";
+import ImageBanner from "../../../../public/sinntax-banner.jpg";
+
+export default function Banner(){
+    return(
+
+    )
+};
