e2ae999385eccac30dba44e32a21580de5013c9d
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-06 19:05:19 +0200

    recebi um componente Banner e usei o state para aplicar o modo escuro globalmente

--- sintax-languages/src/app/page.js
+++ sintax-languages/src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import styles from "./page.module.css";
 import Top from "./Components/Top/Top";
+import Banner from "./Components/Banner/Banner";
 import { useState } from "react";
 
 export default function Home() {
@@ -9,8 +10,11 @@ export default function Home() {
     setLightMode(!lightMode);
   }
   return (
-    <div>
+    <div className={lightMode ? styles.darkMode : styles.lightMode}>
       <Top standardModeLightMode={lightMode} onClickSetLightMode={DarkMode}/>
+      <main className={styles.main}>
+        <Banner />
+      </main>
     </div>
   );
 }
