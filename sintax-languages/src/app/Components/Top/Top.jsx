f1dd1121704096c2380eb97c28bdf00779b40406
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-06 16:58:37 +0200

    escrevi elementos html

--- sintax-languages/src/app/Components/Top/Top.jsx
+++ sintax-languages/src/app/Components/Top/Top.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./Top.module.css";
 import { IoMoonOutline } from "react-icons/io5";
 import { RxSun } from "react-icons/rx";
+import Logo from "../../../../public/sintax-logo.jpg";
 
 export default function Top(){
     return(
-
+        <div className={styles.darkMode}>
+            <header className={styles.header}>
+            <Image src={Logo} alt="logo" title="logo" className={styles.logo}/>
+            <span className={styles.lookSintax}>Veja as sintax de</span>
+            <nav className={styles.navList}>
+                <li className={styles.listItem}><Link href="#" className={styles.listAnchor}>html</Link></li>
+                <li className={styles.listItem}><Link href="#" className={styles.listAnchor}>css</Link></li>
+                <li className={styles.listItem}><Link href="#" className={styles.listAnchor}>js</Link></li>
+                <li className={styles.listItem}><Link href="#" className={styles.listAnchor}>react</Link></li>
+            </nav>
+            <button className={styles.buttonSwitchMode}><IoMoonOutline /></button>
+        </header>
+        </div>
     )
 };
