9ebf0bb0208796312025b7d35c6f8d07a816ca86
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-07 09:13:27 +0200

    criei uma pasta e dentro de outra pasta para imagens e um arquivo

---
+++ sintax-languages/src/app/Components/SintaxOfHtml/array.js
@@ -0,0 +1,11 @@
+import doctype from "./image/!DOCTYPE html.png";
+import html from "./image/html.png";
+import head from "./image/head.png";
+import charset from "./image/meta charset.png";
+ 
+ sintaxOfHtml = [
+    {id: 1, categoria: "meta", texto: "Eu sou um paragrafo  p", sintax: "veja a minha sintax:", imagem: doctype},
+    {id: 2, categoria: "meta", texto: "Eu sou o elemento raiz html", sintax: "veja a minha sintax:", imagem: html},
+    {id: 3, categoria: "meta", texto: "Eu sou o elemento principal head", sintax: "veja a minha sintax:", imagem: head},
+    {id: 4, categoria: "meta", texto: "Eu sou o conjunto de caracteres charset", sintax: "veja a minha sintax:", imagem: charset},
+ ]
