a168d1f3b76e049dbc4abe4180830128b2ed7e4d
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-07 13:09:32 +0200

    adicionei um elemento de titulo

--- sintax-languages/src/app/Components/Banner/Banner.jsx
+++ sintax-languages/src/app/Components/Banner/Banner.jsx
@@ -5,6 +5,7 @@ import styles from "./Banner.module.css";
 export default function Banner(){
     return(
         <section className={styles.banner}>
+            <h1 className={styles.title}>Fábio Tomás Naene</h1>
         </section>
     )
 };
