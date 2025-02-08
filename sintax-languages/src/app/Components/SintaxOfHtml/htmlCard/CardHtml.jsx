d3ab858ec7fc348c378a45a24d61e712652de8da
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-08 17:19:45 +0200

    corrigi a importacao do componente Image

--- sintax-languages/src/app/Components/SintaxOfHtml/htmlCard/CardHtml.jsx
+++ sintax-languages/src/app/Components/SintaxOfHtml/htmlCard/CardHtml.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import styles from "./CardHtml.module.css";
 
 
@@ -7,8 +8,8 @@ export default function CardHtml(props){
     return(
         <div className={styles.card}>
             <p className={styles.title}>{props.titulo} <br></br> {props.tag}</p>
-            <span className={styles.description}>{props.descricao}</span>
-            <Image src={props.sintax} alt={props.titulo} className={styles.img}/>
+            <span className={styles.description}>{props.descricao} <br></br><MdKeyboardDoubleArrowDown /></span>
+            <Image src={props.sintax} alt={props.titulo} title={props.titulo} className={styles.img}/>
         </div>
     )
 };
