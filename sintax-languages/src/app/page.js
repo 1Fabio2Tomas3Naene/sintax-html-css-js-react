6b9e44d9098d4c5ce9492b30b3caaf77e6f98996
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-08 17:23:50 +0200

    criei uma funcao para pesquisar por meio de paravra digitada e usei o array.map para enviar props para o componente card

--- sintax-languages/src/app/page.js
+++ sintax-languages/src/app/page.js
@@ -5,7 +5,9 @@ import styles from "./page.module.css";
 import Top from "./Components/Top/Top";
 import Banner from "./Components/Banner/Banner";
 import { allSintaxOfHtml, searchByTypedWord } from "./Components/SintaxOfHtml/function";
+import { SintaxOfCssAll, searchByTypedWordCss } from "./Components/SintaxOfCss/functios";
 import CardHtml from "./Components/SintaxOfHtml/htmlCard/CardHtml";
+import CardCss from "./Components/SintaxOfCss/CardCss/SintaxOfCss";
 import { FaSearch } from "react-icons/fa";
 
 
@@ -14,6 +16,9 @@ export default function Home() {
   const [sintaxHtml, setSintaxHtml] = useState(allSintaxOfHtml);
   const [typedWord, setTypedWord] =useState("");
 
+  const [sintaxCss, setSintaxCss] = useState(SintaxOfCssAll);
+  const [typedWordCss, setTtpedWordCss] = useState(" ");
+
   const DarkMode = () =>{
     setLightMode(!lightMode);
   }
@@ -21,13 +26,18 @@ export default function Home() {
     setSintaxHtml(searchByTypedWord(paravraDigitada))
     setTypedWord(paravraDigitada)
   }
+
+  const HandleFilterByTypedWordCss =(palavraDigtadaCss) =>{
+    setSintaxCss(searchByTypedWordCss(palavraDigtadaCss))
+    setTtpedWordCss(palavraDigtadaCss);
+  }
   return (
     <div className={lightMode ? styles.darkMode : styles.lightMode}>
       <Top standardModeLightMode={lightMode} onClickSetLightMode={DarkMode}/>
       <main className={styles.main}>
         <Banner />
         <section className={styles.sectionHtml} id="html">
-          <h1 className={styles.title}>Veja as sintax de html</h1>
+          <h2 className={styles.title}>Veja as sintax de html</h2>
           <div className={styles.fieldSearch}>
             <FaSearch className={styles.iconSearch}/>
             <input type="text" placeholder="Digite aqui o nome da tag ou a categoria" value={typedWord} onChange={(Event) => HandleFilterByTypedWord(Event.target.value)} className={styles.input}/>
@@ -47,6 +57,28 @@ export default function Home() {
               })
             }
           </div>
+        </section>
+        <section className={styles.sectionHtml}>
+        <h3 className={styles.tituloCss}>Veja as sintax de css</h3>
+        <div className={styles.fieldSearchCss}>
+            <FaSearch className={styles.iconSearch}/>
+            <input type="text" placeholder="Digite aqui o nome da propriedade ou a categoria" value={typedWordCss} onChange={(Event) => HandleFilterByTypedWordCss(Event.target.value)} className={styles.input}/>
+          </div>
+         <div className={styles.containerCardCss}>
+         {
+            sintaxCss.map((sintaxCssItens) => {
+              return(
+                <CardCss 
+                key={sintaxCssItens.id}
+                textoCss={sintaxCssItens.texto}
+                propriedadeCss={sintaxCssItens.propriedade}
+                sintaxCss={sintaxCssItens.sintax}
+                imagemCss={sintaxCssItens.imagem}
+                />
+              )
+            })
+          }
+         </div>
         </section>
       </main>
     </div>
