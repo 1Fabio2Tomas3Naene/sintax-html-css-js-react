0a87b1a784a479b9e68c7335840c2ddb523b13c0
Fabio Tomas Naene <tomasnaenefabio@gmail.com>
2025-02-14 12:20:32 +0200

    apliquei responsividade

--- sintax-languages-fabio-ftn/src/app/Components/Banner/Banner.jsx
+++ sintax-languages-fabio-ftn/src/app/Components/Banner/Banner.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./Banner.module.css";
 
 export default function Banner(){
     return(
         <section className={styles.banner}>
             <h1 className={styles.title}>Fábio Tomás Naene</h1>
-        </section>
+        </section> 
     )
 };
--- sintax-languages-fabio-ftn/src/app/Components/Top/Top.jsx
+++ sintax-languages-fabio-ftn/src/app/Components/Top/Top.jsx
@@ -5,6 +5,7 @@ import styles from "./Top.module.css";
 import { IoMoonOutline } from "react-icons/io5";
 import { RxSun } from "react-icons/rx";
 import { RiArrowRightDoubleLine } from "react-icons/ri";
+import { RiArrowDownDoubleFill } from "react-icons/ri";
 import Logo from "../../../../public/sintax-logo.svg";
 
 export default function Top(props){
@@ -12,7 +13,7 @@ export default function Top(props){
         <div className={props.standardModeLightMode ? styles.darkMode : styles.lightMode}>
             <header className={styles.header}>
             <Image src={Logo} alt="logo" title="logo" className={styles.logo}/>
-            <span className={styles.lookSintax}>Veja as sintaxes de <RiArrowRightDoubleLine className={styles.arrow}/></span>
+            <span className={styles.lookSintax}>sintaxes de <RiArrowRightDoubleLine className={styles.arrow}/><RiArrowDownDoubleFill className={styles.arrow02}/></span>
             <nav className={styles.navList}>
                 <li className={styles.listItem}><Link href="/#idSintaxHtml" className={styles.listAnchor}>html</Link></li>
                 <li className={styles.listItem}><Link href="/#idSintaxCss" className={styles.listAnchor}>css</Link></li>
