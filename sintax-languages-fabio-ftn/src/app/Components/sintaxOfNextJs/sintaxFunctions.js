c6a4caca60094405e953951980efc19f5ac10e8d
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-02-23 15:48:45 +0200

    por enquanto criei um uma fucao de filtrazem vazia

---
+++ sintax-languages-fabio-ftn/src/app/Components/sintaxOfNextJs/sintaxFunctions.js
@@ -0,0 +1,9 @@
+import { sintaxNextJsArray } from "./sintaxArray";
+
+export const allSintaxNextJs = () => {
+    return sintaxNextJsArray
+};
+
+export const filterByTypedWordNextJs = (typedWordNextJs) => {
+
+};
