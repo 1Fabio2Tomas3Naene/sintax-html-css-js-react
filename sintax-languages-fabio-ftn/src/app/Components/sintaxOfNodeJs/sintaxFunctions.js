29f18686ac5ccadfe5cb9b6827af02acc750f3a4
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-02-23 16:55:35 +0200

    por enquanto criei um uma fucao de filtrazem vazia

---
+++ sintax-languages-fabio-ftn/src/app/Components/sintaxOfNodeJs/sintaxFunctions.js
@@ -0,0 +1,9 @@
+import { sintaxNodeJsArray } from "./sintaxArray";
+
+export const allSintaxNodeJs = () => {
+    return sintaxNodeJsArray
+};
+
+export const filterByTypedWordNodeJs = (typedWordNodeJs) => {
+
+};
