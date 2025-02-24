1232f7cadc2abebd2c95e61480ce8ceb27a6784e
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-02-24 09:24:53 +0200

    importei 10 imagens

--- sintax-languages-fabio-ftn/src/app/Components/sintaxOfGit/sintaxArray.js
+++ sintax-languages-fabio-ftn/src/app/Components/sintaxOfGit/sintaxArray.js
@@ -1,6 +1,13 @@
-
-
-
+import installRecentVersionOfGit from "./assets/installRecentVersionOfGit.svg";
+import checkGitVersion from "./assets/checkGitVersion.svg";
+import configureGitNameEmail from "./assets/configureGitNameEmail.svg";
+import configureGitIfGithubIsPrivate from "./assets/configureGitIfGithubIsPrivate.svg";
+import changeBranchToMain from "./assets/changeBranchToMain.svg";
+import branchForMerge from "./assets/branchForMerge.svg";
+import gitGettingNameAndEmail from "./assets/gitGettingNameAndEmail.svg";
+import showSshKey from "./assets/showSshKey.svg";
+import createNewSshKey from "./assets/createNewSshKey.svg";
+import checkAlgorithmKeyEd2551 from "./assets/checkAlgorithmKeyEd2551.svg"
 
 export const sintaxGitArray = [
 
