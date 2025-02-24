2563f3a5b94ab9a3670f3cbf7ce30b036bc5b925
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-02-24 15:54:06 +0200

    recebi dados do card via props

--- sintax-languages-fabio-ftn/src/app/Components/sintaxOfNodeJs/sintaxCards/cards.jsx
+++ sintax-languages-fabio-ftn/src/app/Components/sintaxOfNodeJs/sintaxCards/cards.jsx
@@ -7,9 +7,9 @@ import styles from "../../SintaxOfHtml/htmlCard/CardHtml.module.css";
 export default function CardNodeJs(props){
     return(
         <div className={styles.card}>
-            <p className={styles.title}>{props.difinitionNodeJs} <br></br> {props.comandNodeJs}</p>
+            <p className={styles.title}>{props.difinitionNodeJs}</p>
             <span className={styles.description}>{props.lookSintaxNodeJs} <br></br><MdKeyboardDoubleArrowDown /></span>
-            <Image src={props.sintaxNextJs} alt={props.comandNodeJs} title={props.difinitionNodeJs} className={styles.img}/>
+            <Image src={props.sintaxNodeJsImage} alt={props.categNodeJs} title={props.difinitionNodeJs} className={styles.img}/>
         </div>
     )
 };
