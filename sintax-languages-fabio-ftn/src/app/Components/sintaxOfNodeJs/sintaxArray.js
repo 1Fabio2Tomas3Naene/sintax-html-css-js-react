77e7fe8c93563c8868717465a5d654ac83834ab6
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-02-24 15:53:28 +0200

    adicionei objetos ao array

--- sintax-languages-fabio-ftn/src/app/Components/sintaxOfNodeJs/sintaxArray.js
+++ sintax-languages-fabio-ftn/src/app/Components/sintaxOfNodeJs/sintaxArray.js
@@ -1,6 +1,21 @@
-
-
+import sudoAptInstallCurl from "./assets/sudoAptInstallCurl.svg";
+import installingNVM from "./assets/installingNVM.svg";
+import inicializandoNVM from "./assets/inicializandoNVM.svg";
+import commandVNvm from "./assets/commandVNvm.svg";
+import nvmInstallLts from "./assets/nvmInstallLts.svg";
+import nvmUseLts from "./assets/nvmUseLts.svg";
+import nodeV from "./assets/nodeV.svg";
+import node from "./assets/node.svg";
+import exit from "./assets/exit.svg";
 
 export const sintaxNodeJsArray = [
-
+    {id: 1, categoria: "instalacao e configuracao o node", texto: "Eu faco com que a instalacao do nvm seja feita corretamente", sintax: "veja a minha sintax", imagem: sudoAptInstallCurl},
+    {id: 2, categoria: "instalacao e configuracao o node", texto: "Eu baixo e instalo o nvm", sintax: "veja a minha sintax", imagem: installingNVM},
+    {id: 3, categoria: "instalacao e configuracao o node", texto: "Eu inicializo o nvm", sintax: "veja a minha sintax", imagem: inicializandoNVM},
+    {id: 4, categoria: "instalacao e configuracao o node", texto: "Eu verifico se o nvm foi instalado", sintax: "veja a minha sintax", imagem: commandVNvm},
+    {id: 5, categoria: "instalacao e configuracao o node", texto: "Eu instalo aversao mais recente do node com suporte a longo prazo LTS", sintax: "veja a minha sintax", imagem: nvmInstallLts},
+    {id: 6, categoria: "instalacao e configuracao o node", texto: "Eu digo ao nvm qual versao do node usar qualdo executarmos o node, neste caso a versao LTS", sintax: "veja a minha sintax", imagem: nvmUseLts},
+    {id: 7, categoria: "instalacao e configuracao o node", texto: "Eu exibo a versao do node, para verificar se foi instalado", sintax: "veja a minha sintax", imagem: nodeV},
+    {id: 8, categoria: "instalacao e configuracao o node", texto: "Eu executo o console do node", sintax: "veja a minha sintax", imagem: node},
+    {id: 9, categoria: "instalacao e configuracao o node", texto: "Eu saio do console do node", sintax: "veja a minha sintax", imagem: exit},
 ];
